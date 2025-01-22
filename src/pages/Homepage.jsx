ac9eb68406ee095cd423d6d2bc8eee48923fe2ae
Paskkal.IO <74718314+Pasify@users.noreply.github.com>
2025-01-22 13:41:42 +0100

    RollupError resolved on Homepage ref of PageNav component

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import styles from "./Homepage.module.css";
 import PageNav from "../components/PageNav";
-
 export default function Homepage() {
   return (
     <main className={styles.homepage}>
